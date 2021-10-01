99feb074376ba3d00d244de146a867ea851f35b5
Lukashevskiy <Dim4ik-super-klass@mail.ru>
2021-10-02 03:31:55 +0800

    all

--- taskforNetwork/banListTelegram/main.cpp
+++ taskforNetwork/banListTelegram/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 pair<string, int> sizeOfMask(string str){
     string ans = "";
     string ans1 = "";
@@ -22,6 +23,7 @@ pair<string, int> sizeOfMask(string str){
     return make_pair(ans1,stoi(ans));
 }
 
+
 unsigned int poW(int n, int i){
     unsigned int ans = 1;
     for(;i>0;i--){
@@ -30,17 +32,25 @@ unsigned int poW(int n, int i){
     return ans;
 }
 
+
 int main(int args, char* argv[]){
+
     ifstream fin(argv[1]);
+
     unsigned int answer = 0;
+
     if(fin.is_open()){
+
         string str;
         while(fin >> str){
             pair<string, int> a;
+
             a = sizeOfMask(str);
             answer += poW(2,32-a.second);
+
             cout << a.first << " ------ " << poW(2,32-a.second) << endl;
         }
+
         cout << "*------------------------------TOTAL------------------------------*" << endl;
         cout << answer << " - ip's blocked\n";
         cout << "*-----------------------------------------------------------------*" << endl;
