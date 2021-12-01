de84afbc76b899060b2505e52b5c0ecb5302bfad
Lukashevskiy <Dim4ik-super-klass@mail.ru>
2021-12-01 08:02:01 +0800

    fix bugs

--- taskforNetwork/translator/decodejson.py
+++ taskforNetwork/translator/decodejson.py
@@ -1,3 +1,4 @@
 import sys, json
+#print(json.load(sys.stdin))
 print(json.load(sys.stdin)[0]['translations'][0]['text'])
 
--- taskforNetwork/translator/translate.sh
+++ taskforNetwork/translator/translate.sh
@@ -3,20 +3,31 @@
 API_KEY="9bf007a897f54e4a94203f8aa88cd2b1"
 REGION="westeurope"
 
-inputfile="./input.txt"
+inputfile=""
 outputfile="./output.txt"
 language="ru"
-
 while [ -n "$1" ]
-do 
-	case $1 in
-		"-l") language=(`$1 | cut -f 2 -d'='`);;
-	       	"-o") outputfile=(`$1 | cut -f 2 -d'='`);;
-		"-i") inputfile=(`$s1 | cut -f 2 -d'='`);;
+do
+	IFS='=' read -ra cname <<< "$1"
+	case ${cname[0]} in
+		'-l') language="${cname[1]}";;
+	       	'-o') outputfile="${cname[1]}";;
+		'-i') inputfile="${cname[1]}";;
 	esac
 	shift
 done
+if [ -z $inputfile ]
+then 
+	echo "Use default input file (input.txt)"
+	inputfile= "./input.txt"	
+fi
 
+
+if [ ! -e $inputfile ]
+then
+	echo "Input file not found"
+	exit
+fi
 text=$(cat $inputfile)
 
 curl -X POST "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=$language" \
@@ -24,5 +35,8 @@ curl -X POST "https://api.cognitive.microsofttranslator.com/translate?api-versio
 	-H "Ocp-Apim-Subscription-Region: $REGION" \
 	-H "Content-Type: application/json" \
 	-d "[{'Text':'\"$text\"'}]" | python3 decodejson.py > $outputfile
+
+
+
 
 
